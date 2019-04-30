af7a1d3faaa4d910f0f05fa660a94d137333e048
MFotouhigit config --global user.email mfotouhi@uw.edugit config --global user.name MFotouhi <mfotouhi@uw.edu>
2019-04-29 19:01:58 -0700

    Need to add relation prediction

--- prediction model/make_predictoins.py
+++ prediction model/make_predictoins.py
@@ -16,3 +16,43 @@ if len(sys.argv) < 3:
     exit()
 input_path = sys.argv[1]
 output_path = sys.argv[2]
+
+#input_path = "C:\\Users\\bhagatsanchya\\Desktop\\UWash\\Machine_Learning\\Project\\python_code\\data\\public-test-data"
+#output_path = "C:\\Users\\bhagatsanchya\\Desktop\\Myoutput"
+#maj_gender_predic_str="female"
+##majority_age="xx-24"
+#extrovert="3.48685789474"
+#neurotic="2.73242421053"
+#agreeable="3.58390421053"
+#conscientious="3.44561684211"
+#openness="3.90869052632"
+
+"""
+    Predictions using text
+"""
+
+# read text input
+print("Reading input")
+input = rp.read_text(input_path)
+
+# age predictions using text
+print("Making age prediction using text")
+age_predictions_text = tcf.make_predictions(input, "naive_bayes", "age", "text")
+
+# personality trait predictions
+personality_predictions = {}
+for personality_trait in tfe.personality_traits:
+    print("Making", personality_trait, "prediction using liwc")
+    personality_predictions[personality_trait] = tcf.make_predictions(input, "linear_regression", personality_trait, "liwc")
+
+# gender predictions
+print("Making gender prediction using text")
+gender_predictions_text = tcf.make_predictions(input, "naive_bayes", "gender", "text")
+
+'''
+    Predictions using image
+'''
+
+print("Making gender prediction using image")
+print(input_path)
+gender_predictions_image = icc.make_predictions(input_path, "gender")
